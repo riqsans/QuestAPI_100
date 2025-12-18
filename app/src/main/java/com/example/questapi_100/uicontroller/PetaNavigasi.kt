7d417934cebd47fd317dd60501124970952b3cba
riqsans <thrq88@gmail.com>
2025-12-18 16:00:36 +0700

    membuat PetaNavigasi dan HostNavigasi untuk routing aplikasi

---
+++ app/src/main/java/com/example/questapi_100/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,37 @@
+package com.example.questapi_100.uicontroller
+
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(),
+                 modifier: Modifier) {
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(navController = navController, startDestination = DestinasiHome.route,
+        modifier = modifier) {
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                navigateToItemUpdate = {
+                    navController.navigate("${DestinasiDetail.route}/${it}")
+                })
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) })
+        }
+    }
+}
