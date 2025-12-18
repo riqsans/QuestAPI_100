c450c1a86702bdfe589f23eb9e133ea784302666
riqsans <thrq88@gmail.com>
2025-12-18 15:51:08 +0700

    membuat DestinasiHome sebagai route awal aplikasi

---
+++ app/src/main/java/com/example/questapi_100/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.questapi_100.uicontroller.route
+
+import com.example.questapi_100.R
+
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
