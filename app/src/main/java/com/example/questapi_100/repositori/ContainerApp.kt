0be549a1aad9bec1ebe35fdad314372638841e8d
riqsans <thrq88@gmail.com>
2025-12-18 16:47:24 +0700

    mengubah baseurl menjadi 10.0.2.2 untuk akses localhost emulator

--- app/src/main/java/com/example/questapi_100/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/questapi_100/repositori/ContainerApp.kt
@@ -15,7 +15,7 @@ interface ContainerApp{
 }
 
 class DefaultContainerApp : ContainerApp {
-    private val baseurl = "http://127.0.0.1/tiumy/"
+    private val baseurl = "http://10.0.2.2/tiumy/"
 
     val logging = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
