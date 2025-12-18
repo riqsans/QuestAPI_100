5f4edf49436bb0d925cf95b179cc18a676c19c48
riqsans <thrq88@gmail.com>
2025-12-18 13:05:56 +0700

    menambahkan PenyediaViewModel object dengan Factory untuk HomeViewModel dan EntryViewModel

--- app/src/main/java/com/example/questapi_100/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questapi_100/viewmodel/provider/PenyediaViewModel.kt
@@ -1,2 +1,25 @@
 package com.example.questapi_100.viewmodel.provider
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.questapi_100.repositori.AplikasiDataSiswa
+import com.example.questapi_100.viewmodel.EntryViewModel
+import com.example.questapi_100.viewmodel.HomeViewModel
+import kotlinx.coroutines.channels.Channel
+import okhttp3.Call
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa)
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
+        }
+    }
+}
