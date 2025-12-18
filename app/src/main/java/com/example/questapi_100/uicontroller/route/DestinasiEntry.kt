020403e8fdcafbd0b366833669a9f357105e6750
riqsans <thrq88@gmail.com>
2025-12-18 15:50:48 +0700

    membuat object DestinasiEntry untuk route navigasi entry siswa

---
+++ app/src/main/java/com/example/questapi_100/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.questapi_100.uicontroller.route
+
+import com.example.questapi_100.R
+
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
