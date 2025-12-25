9b05748580713b9bbab4cacc5d46bb92832446ac
riqsans <thrq88@gmail.com>
2025-12-25 13:35:57 +0700

    menambahkan route untuk DestinasiDetail pada PetaNavigasi

--- app/src/main/java/com/example/questapi_100/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/questapi_100/uicontroller/PetaNavigasi.kt
@@ -9,6 +9,7 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.questapi_100.uicontroller.route.DestinasiDetail
 import com.example.questapi_100.uicontroller.route.DestinasiEntry
 import com.example.questapi_100.uicontroller.route.DestinasiHome
 import com.example.questapi_100.view.EntrySiswaScreen
@@ -35,6 +36,10 @@ fun HostNavigasi(
         composable(DestinasiEntry.route){
             EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) })
         }
-
+        composable(
+            DestinasiDetail.routeWithArgs,arguments = listOf(navArgument(DestinasiDetail
+            .itemIdArg) {
+            type = NavType.IntType })
+        )
     }
 }
