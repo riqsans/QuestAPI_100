8a5b6fef461bba9c1fb20ee9f30f6153834790ac
riqsans <thrq88@gmail.com>
2025-12-25 14:26:48 +0700

    membuat fungsi composable DetailSiswaScreen di HalamanDetail

--- app/src/main/java/com/example/questapi_100/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi_100/view/HalamanDetail.kt
@@ -1,4 +1,12 @@
 package com.example.questapi_100.view
 
-class HalamanDetail {
-}
+import androidx.compose.material3.ExperimentalMaterial3Api
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailSiswaScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailSiswaViewModel = viewModel(factory = PenyediaViewModel.Factory)
+)
