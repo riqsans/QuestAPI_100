bf9b97c15fffea7c954d4fbad27a14c7abf3fb69
riqsans <thrq88@gmail.com>
2025-12-25 17:21:22 +0700

    menambahkan import dan implementasi UI untuk DetailSiswaScreen

--- app/src/main/java/com/example/questapi_100/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi_100/view/HalamanDetail.kt
@@ -1,3 +1,46 @@
+package com.example.questapi_100.view
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi_100.R
+import com.example.questapi_100.modeldata.DataSiswa
+import com.example.questapi_100.uicontroller.route.DestinasiDetail
+import com.example.questapi_100.viewmodel.DetailViewModel
+import com.example.questapi_100.viewmodel.StatusUIDetail
+import com.example.questapi_100.viewmodel.provider.PenyediaViewModel
+import kotlinx.coroutines.launch
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailSiswaScreen(
