09d6bc651c9dd1910fd574c351a4d49179b04350
riqsans <thrq88@gmail.com>
2025-12-25 14:43:40 +0700

    memperbarui full kode HalamanDetail.kt yang sebelumnya salah

--- app/src/main/java/com/example/questapi_100/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi_100/view/HalamanDetail.kt
@@ -1,71 +1,68 @@
-package com.example.questapi_100.view
-
-import androidx.compose.material3.ExperimentalMaterial3Api
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailSiswaScreen(
     navigateToEditItem: (Int) -> Unit,
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
-    viewModel: DetailSiswaViewModel = viewModel(factory = PenyediaViewModel.Factory)
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
-    val uiState = viewModel.uiStateDetail.collectAsState()
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
-
     Scaffold(
-        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
             SiswaTopAppBar(
                 title = stringResource(DestinasiDetail.titleRes),
                 canNavigateBack = true,
-                navigateUp = navigateBack,
-                scrollBehavior = scrollBehavior
+                navigateUp = navigateBack
             )
         },
         floatingActionButton = {
+            val uiState = viewModel.statusUIDetail
             FloatingActionButton(
-                onClick = { navigateToEditItem(uiState.value.detailSiswa.id) },
+                onClick = {
+                    when(uiState){ is StatusUIDetail.Success->
+                        navigateToEditItem(uiState.satusiswa.id) else->{}}
+                },
                 shape = MaterialTheme.shapes.medium,
                 modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+
             ) {
                 Icon(
                     imageVector = Icons.Default.Edit,
-                    contentDescription = stringResource(R.string.edit_siswa),
+                    contentDescription = stringResource(R.string.update),
                 )
             }
-        },
+        }, modifier = modifier
     ) { innerPadding ->
-        BodyDetailSiswa(
-            detailSiswaUiState = uiState.value,
-            onDelete = {
-                viewModel.deleteItem()
+        val coroutineScope = rememberCoroutineScope()
+        BodyDetailDataSiswa(
+            statusUIDetail = viewModel.statusUIDetail,
+            onDelete = { coroutineScope.launch {
+                viewModel.hapusSatuSiswa()
                 navigateBack()
-            },
-            modifier = Modifier.padding(innerPadding)
+            }},
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
         )
     }
 }
 
 @Composable
-fun BodyDetailSiswa(
-    detailSiswaUiState: DetailSiswaUiState,
+private fun BodyDetailDataSiswa(
+    statusUIDetail: StatusUIDetail,
     onDelete: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = modifier
-            .padding(dimensionResource(id = R.dimen.padding_medium))
-            .verticalScroll(rememberScrollState()),
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
         verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
     ) {
         var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
-
-        ItemDetailSiswa(
-            siswa = detailSiswaUiState.detailSiswa,
-            modifier = Modifier.fillMaxWidth()
-        )
-
+        when(statusUIDetail){
+            is StatusUIDetail.Success -> DetailDataSiswa(
+                siswa = statusUIDetail.satusiswa,
+                modifier = Modifier.fillMaxWidth())
+            else -> {}
+        }
         OutlinedButton(
             onClick = { deleteConfirmationRequired = true },
             shape = MaterialTheme.shapes.small,
@@ -73,7 +70,6 @@ fun BodyDetailSiswa(
         ) {
             Text(stringResource(R.string.delete))
         }
-
         if (deleteConfirmationRequired) {
             DeleteConfirmationDialog(
                 onDeleteConfirm = {
@@ -88,12 +84,11 @@ fun BodyDetailSiswa(
 }
 
 @Composable
-fun ItemDetailSiswa(
-    siswa: Siswa, modifier: Modifier = Modifier
+fun DetailDataSiswa(
+    siswa: DataSiswa, modifier: Modifier = Modifier
 ) {
     Card(
-        modifier = modifier,
-        colors = CardDefaults.cardColors(
+        modifier = modifier, colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer,
             contentColor = MaterialTheme.colorScheme.onPrimaryContainer
         )
@@ -104,46 +99,50 @@ fun ItemDetailSiswa(
                 .padding(dimensionResource(id = R.dimen.padding_medium)),
             verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
         ) {
-            ComponentDetailSiswa(
-                judul = R.string.id,
-                isinya = siswa.id.toString(),
+            BarisDetailData(
+                labelResID = R.string.nama1,
+                itemDetail = siswa.nama,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(
+                        id = R.dimen
+                            .padding_medium
+                    )
+                )
             )
-            ComponentDetailSiswa(
-                judul = R.string.nama,
-                isinya = siswa.nama,
+            BarisDetailData(
+                labelResID = R.string.alamat1,
+                itemDetail = siswa.alamat,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(
+                        id = R.dimen
+                            .padding_medium
+                    )
+                )
             )
-            ComponentDetailSiswa(
-                judul = R.string.alamat,
-                isinya = siswa.alamat,
-            )
-            ComponentDetailSiswa(
-                judul = R.string.telpon,
-                isinya = siswa.telpon,
+            BarisDetailData(
+                labelResID = R.string.telpon1,
+                itemDetail = siswa.telpon,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(
+                        id = R.dimen
+                            .padding_medium
+                    )
+                )
             )
         }
     }
 }
 
 @Composable
-fun ComponentDetailSiswa(
-    @StringRes judul: Int,
-    isinya: String,
-    modifier: Modifier = Modifier
+private fun BarisDetailData(
+    @StringRes labelResID: Int, itemDetail: String, modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier
-    ) {
-        Text(
-            text = stringResource(id = judul),
-            style = MaterialTheme.typography.labelMedium
-        )
-        Text(
-            text = isinya,
-            style = MaterialTheme.typography.bodyLarge,
-        )
+    Row(modifier = modifier) {
+        Text(stringResource(labelResID))
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = itemDetail, fontWeight = FontWeight.Bold)
     }
 }
-
 @Composable
 private fun DeleteConfirmationDialog(
     onDeleteConfirm: () -> Unit,
@@ -152,17 +151,16 @@ private fun DeleteConfirmationDialog(
 ) {
     AlertDialog(onDismissRequest = { /* Do nothing */ },
         title = { Text(stringResource(R.string.attention)) },
-        text = { Text(stringResource(R.string.delete)) },
+        text = { Text(stringResource(R.string.tanya)) },
         modifier = modifier,
         dismissButton = {
             TextButton(onClick = onDeleteCancel) {
-                Text(text = stringResource(R.string.no))
+                Text(stringResource(R.string.no))
             }
         },
         confirmButton = {
             TextButton(onClick = onDeleteConfirm) {
-                Text(text = stringResource(R.string.yes))
+                Text(stringResource(R.string.yes))
             }
-        }
-    )
+        })
 }
