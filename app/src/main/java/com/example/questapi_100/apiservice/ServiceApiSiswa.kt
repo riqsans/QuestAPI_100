5d9569b243832ab1c05202051cadf5859f66c446
riqsans <thrq88@gmail.com>
2025-12-25 13:12:37 +0700

    menambahkan endpoint editSatuSiswa di ServiceApiSiswa interface

--- app/src/main/java/com/example/questapi_100/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_100/apiservice/ServiceApiSiswa.kt
@@ -18,4 +18,8 @@ interface ServiceApiSiswa {
     @GET("bacaTeman1.php/{id}")
     suspend fun getSatuSiswa(@Query("id") id:Int):DataSiswa
 
+    @PUT("editTM.php/{id}")
+    suspend fun editSatuSiswa(@Query("id") id:Int, @Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+
+
 }
