6432f34c6f0473ba844286cb2ee70bb86d4912d4
riqsans <thrq88@gmail.com>
2025-12-25 17:21:05 +0700

    menambahkan route destinasi untuk edit siswa

---
+++ app/src/main/java/com/example/questapi_100/uicontroller/route/DestinasiEdit.kt
@@ -0,0 +1,10 @@
+package com.example.questapi_100.uicontroller.route
+
+import com.example.questapi_100.R
+
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "itemId"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
