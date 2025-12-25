a440982fa623685e1de23d3c55e362f9feb8a792
riqsans <thrq88@gmail.com>
2025-12-25 14:13:01 +0700

    menambahkan state interface StatusUIDetail untuk DetailViewModel

--- app/src/main/java/com/example/questapi_100/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_100/viewmodel/DetailViewModel.kt
@@ -1,4 +1,23 @@
 package com.example.questapi_100.viewmodel
 
-class DetailViewModel {
+import android.annotation.SuppressLint
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_100.modeldata.DataSiswa
+import com.example.questapi_100.repositori.RepositoryDataSiswa
+import com.example.questapi_100.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.HttpException
+import retrofit2.Response
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
 }
+
